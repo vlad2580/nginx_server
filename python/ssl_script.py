bb941ffa7081829fbe44b43fb1620eab44e08baa
Vladislav Yurikov <yurikov.v@tutanota.com>
2023-09-19 04:27:18 +0200

    ssl comments

--- python/ssl_script.py
+++ python/ssl_script.py
@@ -1,30 +1,30 @@
 import fileinput
 import os
 
+# Path to the Nginx configuration file
 nginx_config_file = "/etc/nginx/sites-available/default"
 
-# Пути к сертификату и ключу SSL
+# Paths to the SSL certificate and key
 certificate_path = "/etc/nginx/ssl/www.mycomp.local+4.pem"
 private_key_path = "/etc/nginx/ssl/www.mycomp.local+4-key.pem"
 
-# Читаем файл конфигурации и создаем временный файл
+# Read the configuration file and create a temporary file
 with fileinput.input(nginx_config_file, inplace=True, backup='.bak') as file:
     add_ssl_paths = False
     for line in file:
-        # Если строка содержит "listen [::]:443 ssl default_server;"
+        # If the line contains "listen [::]:443 ssl default_server;"
         if line.strip() == "# listen [::]:443 ssl default_server;" or \
            line.strip() == "listen [::]:443 ssl default_server;":
             add_ssl_paths = True
 
-        # Печатаем текущую строку
+        # Print the current line
         print(line, end='')
 
-        # Если найдена строка с SSL-конфигурацией, добавляем пути к сертификату и ключу
+        # If a line with SSL configuration is found, add the paths to the certificate and key
         if add_ssl_paths:
             print(f"        ssl_certificate {certificate_path};")
             print(f"        ssl_certificate_key {private_key_path};")
             add_ssl_paths = False
 
-            
-# После выполнения всех операций, перезапускаем Nginx
+# After completing all operations, restart Nginx
 os.system("sudo systemctl restart nginx")
