2dde857dec878452f4a2d2cca57c78062b7a319a
Vladislav Yurikov <yurikov.v@tutanota.com>
2023-09-19 04:25:50 +0200

    docker comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Перемещаемся в директорию с файлом test.tf
+# Navigate to the directory containing the test.tf file
 cd /root/terraform
 
-# Инициализируем Terraform
+# Initialize Terraform
 terraform init
 
-# Применяем конфигурацию Terraform
-terraform apply -auto-approve
+# Apply the Terraform configuration
+terraform apply -auto-approve 
 
 docker run -it --rm terraform-container bash -c "cd /root/terraform && terraform init && terraform apply"
