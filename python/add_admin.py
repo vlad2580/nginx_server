e3ecb0a57dbd530ba64bc7b9ede50817a004a5d5
Vladislav Yurikov <yurikov.v@tutanota.com>
2023-09-19 14:53:11 +0200

    add_admin.py new release

--- python/add_admin.py
+++ python/add_admin.py
@@ -1,31 +1,34 @@
 import fileinput
 import os
 
-# Path to the Nginx configuration file
+# Nginx configuration file path
 nginx_config_file = "/etc/nginx/sites-available/default"
 
-# Paths to the SSL certificate and key
-certificate_path = "/etc/nginx/ssl/www.mycomp.local+4.pem"
-private_key_path = "/etc/nginx/ssl/www.mycomp.local+4-key.pem"
+# Create a string for adding the location /admin block
+new_location_block = """
+        location /admin {
+                auth_basic "Restricted Access";
+                auth_basic_user_file /etc/nginx/.htpasswd;
+                alias /home/ubuntu/web;
+                index admin.html;
+        }
+"""
 
 # Read the configuration file and create a temporary file
 with fileinput.input(nginx_config_file, inplace=True, backup='.bak') as file:
-    add_ssl_paths = False
+    add_location_admin = False
     for line in file:
-        # If the line contains "listen [::]:443 ssl default_server;"
-        if line.strip() == "# listen [::]:443 ssl default_server;" or \
-           line.strip() == "listen [::]:443 ssl default_server;":
-            add_ssl_paths = True
+        # Set a flag if the start of the location / block is found
+        if line.strip() == "location / {":
+            add_location_admin = True
 
         # Print the current line
         print(line, end='')
 
-        # If a line with SSL configuration is found, add the paths to the certificate and key
-        if add_ssl_paths:
-            print(f"        ssl_certificate {certificate_path};")
-            print(f"        ssl_certificate_key {private_key_path};")
-            add_ssl_paths = False
+        # If the closing of the location / block is found, add the location /admin block after it
+        if add_location_admin and line.strip() == "}":
+            print(new_location_block)
+            add_location_admin = False
 
-# After completing all operations, restart Nginx
+# After all operations are completed, restart Nginx
 os.system("sudo systemctl restart nginx")
-
