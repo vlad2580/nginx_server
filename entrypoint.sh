46eed9e232720f49ee264680f2057b3140d9170e
Vladislav Yurikov <yurikov.v@tutanota.com>
2023-09-19 15:17:19 +0200

    Getting access,secret key and region from user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-# Navigate to the directory containing the test.tf file
+# Запрос данных AWS у пользователя
+read -p "Введите AWS Access Key ID: " access_key
+read -p "Введите AWS Secret Access Key: " secret_key
+read -p "Введите AWS регион: " region
+
+# Устанавливаем полученные данные как переменные окружения
+export AWS_ACCESS_KEY_ID="$access_key"
+export AWS_SECRET_ACCESS_KEY="$secret_key"
+export AWS_DEFAULT_REGION="$region"
+
+# Переход в директорию с файлом test.tf
 cd /root/terraform
 
-# Initialize Terraform
+# Инициализация Terraform
 terraform init
 
-# Apply the Terraform configuration
-terraform apply -auto-approve 
+# Применение конфигурации Terraform
+terraform apply -auto-approve
 
-docker run -it --rm terraform-container bash -c "cd /root/terraform && terraform init && terraform apply"
+# Запуск других команд, если необходимо
+# Например, выполнение других скриптов
+
+# Ваш код для выполнения других команд
+
+# Сообщение о завершении работы
+echo "Работа завершена"
+
+# Завершение работы контейнера
+exit 0
